07017287a749f53a7adf266f9f894814279b0e56
Chad Mottershead <phageogram@gmail.com>
2024-12-23 17:28:51 +0000

    added APIConnectionError exception class

--- pipeline/api/exceptions.py
+++ pipeline/api/exceptions.py
@@ -1,3 +1,3 @@
 class APIClientError:
-    def __init__(self):
-        
+    def __init__(self, e):
+        raise ConnectionError(f"Error connecting to OpenAQ API: {e}")
--- tests/test_countries_processor.py
+++ tests/test_countries_processor.py
@@ -1,10 +1,16 @@
 import os
 import pandas as pd
+from pipeline.api.client import OpenAQClient
 from pipeline.processors.countries_processor import CountryProcessor
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def test_country_processor():
-    sample_data = './tests/countries_output.csv'
-    df = pd.read_csv(sample_data)
+    api_key = os.getenv("OPENAQ-API-KEY")
+    client = OpenAQClient(api_key=api_key)
+    sample_data = client.fetch_data(endpoint="countries", limit=1)
+    df = pd.DataFrame(sample_data)
     processor = CountryProcessor(df)
     new_df = processor.process()
     print(new_df.head())
